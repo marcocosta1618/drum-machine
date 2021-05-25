2492b71763af7d4191432b63442a18d3c7f14d7f
marcocosta1618 <marcocosta1618@gmail.com>
2021-05-25 12:09:47 +0200

    Add lightning on keypress
    Add .press class to _drum-pad.scss
    Add .press class toggle in DrumPad.js
    
    Signed-off-by: marcocosta1618 <marcocosta1618@gmail.com>

--- src/components/DrumPad.js
+++ src/components/DrumPad.js
@@ -5,8 +5,9 @@ export default function DrumPad(props) {
 
     // create a reference for html audio element
     const audioEl = useRef(null);
+    const buttonEl = useRef(null);
 
-    const play_UpdateDisplay = (e) => {
+    const playAndDisplay = (e) => {
         const sound = audioEl.current;
         sound.currentTime = 0;
         // play sound and prevent Uncaught(in promise) DOMException when running
@@ -19,10 +20,13 @@ export default function DrumPad(props) {
                 })
         }
         props.updateDisplay(e.target.id);
+        const button = buttonEl.current;
+        button.classList.toggle('press');
+        setTimeout(() => button.classList.toggle('press'), 75);
     }
 
     return (
-        <button id={props.id} className="drum-pad" onClick={play_UpdateDisplay} >
+        <button id={props.id} className="drum-pad" ref={buttonEl} onClick={playAndDisplay} >
             {props.padLabel}
             <audio id={props.padLabel} className="clip" ref={audioEl} src={props.url}></audio>
         </button>
