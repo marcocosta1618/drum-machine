b1c204fcbfb0578d0b0eee24504e296eaf8a908a
marcocosta1618 <marcocosta1618@gmail.com>
2021-05-27 17:17:04 +0200

    Add material-UI volume icons and styling
    Signed-off-by: marcocosta1618 <marcocosta1618@gmail.com>

--- src/components/Volume.js
+++ src/components/Volume.js
@@ -1,11 +1,19 @@
 import Slider from '@material-ui/core/Slider';
+import VolumeDownIcon from '@material-ui/icons/VolumeDown';
+import VolumeUpIcon from '@material-ui/icons/VolumeUp';
 
 export default function Volume(props) {
 
     return (
-        <Slider value={props.volume} 
-                onChange={props.handleChange} 
+        <div>
+            <VolumeDownIcon className={props.power ? "on" : "off"} />
+            <Slider 
+                value={props.volume}
+                onChange={props.handleChange}
                 disabled={!props.power}
-        />
+                className={props.power ? "on" : "off"}
+            />
+            <VolumeUpIcon className={props.power ? "on" : "off"} />
+        </div>
     )
 }
