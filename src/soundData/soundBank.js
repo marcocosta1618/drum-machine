887c9e6be16b8d6eefc4dbae60acc633d3aa321c
marcocosta1618 <marcocosta1618@gmail.com>
2021-05-29 14:12:43 +0200

    Change reference to audio files as mp3
    Signed-off-by: marcocosta1618 <marcocosta1618@gmail.com>

--- src/soundData/soundBank.js
+++ src/soundData/soundBank.js
@@ -2,47 +2,47 @@ const soundBank = [
     {
         padLabel: "Q",
         id: "chord 1",
-        url: "/sound/Chord 1.wav"
+        url: "./sound/Chord 1.mp3"
     },
     {
         padLabel: "W",
         id: "chord 2",
-        url: "/sound/Chord 2.wav"
+        url: "./sound/Chord 2.mp3"
     },
     {
         padLabel: "E",
         id: "chord 3",
-        url: "/sound/Chord 3.wav"
+        url: "./sound/Chord 3.mp3"
     },
     {
         padLabel: "A",
         id: "closed HH 909",
-        url: "/sound/Closed HH 909.wav"
+        url: "./sound/Closed HH 909.mp3"
     },
     {
         padLabel: "S",
         id: "open HH 909",
-        url: "/sound/OpenHH 909.wav"
+        url: "./sound/OpenHH 909.mp3"
     },
     {
         padLabel: "D",
         id: "rimshot",
-        url: "/sound/Rimshot.wav"
+        url: "./sound/Rimshot.mp3"
     },
     {
         padLabel: "Z",
         id: "kick 909",
-        url: "/sound/Kick 909.wav"
+        url: "./sound/Kick 909.mp3"
     },
     {
         padLabel: "X",
         id: "snare 909-1",
-        url: "/sound/Snare 909_1.wav"
+        url: "./sound/Snare 909_1.mp3"
     },
     {
         padLabel: "C",
         id: "snare 909-2",
-        url: "/sound/Snare 909_2.wav"
+        url: "./sound/Snare 909_2.mp3"
     }
 ]
 
