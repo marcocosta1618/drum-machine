1980306a1989a9c1099783a8b64c3f5a60613153
marcocosta1618 <marcocosta1618@gmail.com>
2021-05-29 18:46:09 +0200

    Fix drum pads div container.
    Change display from flex to grid.
    
    Signed-off-by: marcocosta1618 <marcocosta1618@gmail.com>

--- src/components/PadBank.js
+++ src/components/PadBank.js
@@ -9,41 +9,41 @@ import soundBank from '../soundData/soundBank';
 // PadBank creates a DrumPad for each object in soundBank array
 function PadBank(props) {
 
-    const [ soundLabel, setSoundLabel ] = useState("");
-    const updateDisplay = function(e) {
+    const [soundLabel, setSoundLabel] = useState("");
+    const updateDisplay = function (e) {
         setSoundLabel(e);
     }
 
-    const [ volume, setVolume ] = useState(40);
-    const handleChange = function(e, value) {
+    const [volume, setVolume] = useState(40);
+    const handleChange = function (e, value) {
         setVolume(value);
     }
 
     return (
         <div id="padBank">
-            <Display soundLabel={soundLabel} 
-                     volume={volume} 
-                     power={props.power}
+            <Display soundLabel={soundLabel}
+                volume={volume}
+                power={props.power}
             />
             {/* return a DrumPad component for each object in soundBank */}
             <div id="pad-div">
-            {soundBank.map((soundData) => {
-                return (
-                    <DrumPad
-                        updateDisplay={updateDisplay}
-                        padLabel={soundData.padLabel}
-                        volume={volume}
-                        power={props.power}
-                        id={soundData.id}
-                        key={soundData.id} // unique key attribute for React
-                        url={soundData.url} 
-                    />
-                )
-            })}
+                {soundBank.map((soundData) => {
+                    return (
+                        <DrumPad
+                            updateDisplay={updateDisplay}
+                            padLabel={soundData.padLabel}
+                            volume={volume}
+                            power={props.power}
+                            id={soundData.id}
+                            key={soundData.id} // unique key attribute for React
+                            url={soundData.url}
+                        />
+                    )
+                })}
             </div>
-            <Volume volume={volume} 
-                    handleChange={handleChange} 
-                    power={props.power}
+            <Volume volume={volume}
+                handleChange={handleChange}
+                power={props.power}
             />
         </div>
     )
